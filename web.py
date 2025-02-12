d506228135af06e2470594eb23addc1071f3f15b
Sayantan Chaudhuri <sayantan86701@gmail.com>
2025-02-12 15:16:09 +0530

    web.py

--- web.py
+++ web.py
@@ -3,31 +3,24 @@ import tensorflow as tf
 import numpy as np
 from PIL import Image
 
-# Load the model once when the app starts
 model = tf.keras.models.load_model("trained_plant_disease.keras")
 
 def model_prediction(test_image):
-    # Open image from the uploaded file and preprocess it
     image = tf.keras.preprocessing.image.load_img(test_image, target_size=(128, 128))
     input_arr = tf.keras.preprocessing.image.img_to_array(image)
     input_arr = np.array([input_arr])
     predictions = model.predict(input_arr)
     return np.argmax(predictions)
 
-# Sidebar title and page selection
 st.sidebar.title("POTATO LEAF DISEASE DETECTION 🌱")
 app_mode = st.sidebar.selectbox('Select page', ['Home', 'Disease Recognition'])
 
-# Home page
 if app_mode == 'Home':
-    # Add an image at the top
-    home_image = Image.open('leaf.jpg')  # Update the path
+    home_image = Image.open('leaf.jpg') 
     st.image(home_image, caption="Potato Plant (Healthy vs Diseased)", use_container_width=True)
 
-    # Title and Description
     st.markdown("<h1 style='text-align: center;'>POTATO LEAF DISEASE DETECTION </h1>", unsafe_allow_html=True)
     
-    # Add some description and instructions
     st.markdown("""
     This system uses a machine learning model to detect plant diseases in potato crops. 
     By uploading an image of a potato plant's leaf, the system will predict whether the plant is healthy or affected by a disease.
@@ -48,22 +41,17 @@ if app_mode == 'Home':
     ### Let's start by uploading an image in the 'Disease Recognition' page.
     """)
 
-# Disease Recognition page
 elif app_mode == 'Disease Recognition':
     st.header('POTATO LEAF DISEASE DETECTION ')
 
-    # File uploader widget
     test_image = st.file_uploader('Choose an image:', type=['jpg', 'png', 'jpeg'])
 
-    # If an image is uploaded
     if test_image:
-        # Show the uploaded image
         st.image(test_image, width=400, use_container_width=True)
 
-        # Button to trigger prediction
         if st.button('Predict'):
-            st.snow()  # Show snow animation
+            st.snow() 
             st.write('Prediction in progress...')
-            result_index = model_prediction(test_image)  # Get prediction from the model
+            result_index = model_prediction(test_image)  
             class_name = ['Potato__Early_blight', 'Potato_Late_blight', 'Potato__healthy']
-            st.success(f'Model predicts it is: {class_name[result_index]}')  # Show prediction result
+            st.success(f'Model predicts it is: {class_name[result_index]}')  
